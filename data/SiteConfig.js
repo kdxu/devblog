9f61c9d4a6549db00c93f62d45f4e166b5720d06
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-06-11 00:45:52 +0900

    fix configs

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,15 +1,13 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogPostDir: "posts", // The name of directory that contains your posts.
   siteTitle: "kdxu devblog", // Site title.
   siteTitleAlt: "kdxu dev log", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://kdxu.github.io/devblog", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://kdxu.github.io", // Domain of your website without pathPrefix.
+  pathPrefix: "kdxu.github.io/devblog", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
   disqusShortname: "https-kdxu-github-io-gatsby-material-starter", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "kdxu", // Username to display in the author segment.
