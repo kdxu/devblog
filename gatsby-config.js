65ebfa5d2a1cab8d1a224136ffd7778e8df015eb
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-09-24 00:56:44 +0900

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-lodash",
     "gatsby-plugin-sass",
+     "gatsby-plugin-netlify-cms",
     {
       resolve: "gatsby-source-filesystem",
       options: {
