7f35a4683cfe4878f7409e6bb6c05cd21f403857
Kyoko KADOWAKI <kyoko.kadowaki+github@gmail.com>
2024-03-25 11:01:35 +0900

    update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteTitle: "kdxu dev blog", // Site title.
-  siteTitleAlt: "kdxu dev blog", // Alternative site title for SEO.
+  siteTitle: "kyoko-kadowaki dev blog", // Site title.
+  siteTitleAlt: "dev blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://devblog.fornothing.net", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
@@ -11,9 +11,7 @@ module.exports = {
   siteRss: "/rss.xml", // Path to the RSS file.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "kdxu", // Username to display in the author segment.
-  userTwitter: "kdxu", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Tokyo", // User location to display in the author segment.
-  userAvatar:
+  userLocation: "Ibaraki", // User location to display in the author segment.
     "https://kdxu.fornothing.net/4aa888496036495f68a5c51dc89d3477.png", // User avatar to display in the author segment.
   userDescription: "fixme", // User description to display in the author segment
   userLinks: [
@@ -22,11 +20,6 @@ module.exports = {
       url: "https://github.com/kdxu",
       iconClassName: "fa fa-github"
     },
-    {
-      label: "Twitter",
-      url: "https://twitter.com/kdxu",
-      iconClassName: "fa fa-twitter"
-    }
   ],
   copyright: "" // Copyright string for the footer of the website and RSS feed.
 };
